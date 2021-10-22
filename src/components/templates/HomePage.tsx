1b4e3f87b4c84323a5f2829d24e3cb0609d7f126
Juliana Rocha <julianarocha@MMBA-B403.local>
2021-10-22 16:44:28 -0300

    Basic Structure

---
+++ src/components/templates/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const HomePage = () => {
+    return (
+        <div className='HomePage--Container'>
+            <h1>Here is the HomePage</h1>
+        </div>
+    )
+}
